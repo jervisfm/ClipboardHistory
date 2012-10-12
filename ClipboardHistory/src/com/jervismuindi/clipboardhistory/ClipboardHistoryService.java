407da1350a170dc0ae0c9f8cce438eed8ecc2532
Jervis Muindi <jervis>
2012-10-11 23:30:41 -0400

    rough quick test to see if my idea works. tested it and it works !

---
+++ ClipboardHistory/src/com/jervismuindi/clipboardhistory/ClipboardHistoryService.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.jervismuindi.clipboardhistory;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+/**
+ * @author Jervis
+ *
+ */
+public class ClipboardHistoryService extends IntentService {
+
+	public ClipboardHistoryService() {
+		super("ClipboardHistoryService");
+		// TODO Auto-generated constructor stub
+	}
+
+	/* 
+	 * 
+	 */
+	@Override
+	protected void onHandleIntent(Intent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- ClipboardHistory/src/com/jervismuindi/clipboardhistory/MainActivity.java
+++ ClipboardHistory/src/com/jervismuindi/clipboardhistory/MainActivity.java
@@ -1,21 +1,74 @@
 package com.jervismuindi.clipboardhistory;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+
 import android.app.Activity;
+import android.content.ClipData;
+import android.content.ClipDescription;
+import android.content.ClipboardManager;
+import android.content.ClipboardManager.OnPrimaryClipChangedListener;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	private ArrayList<String> mClips = new ArrayList<String>(10);
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getActionBar().setDisplayHomeAsUpEnabled(true);
-        //
+        TextView tv = (TextView) findViewById(R.id.HelloText);
+        tv.setText("Changed Text");
+        startMonitoringClipboard();         
     }
 
+    public void showMessage(String msg){
+    	Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+    
+    public void showLatestClipboard() {
+    	 TextView tv = (TextView) findViewById(R.id.HelloText);
+    	 StringBuffer sb = new StringBuffer();
+    	 sb.append("Current Clipboard State:\n");
+    	 for(String s : mClips) {
+    		 sb.append(s + "\n"); 
+    	 }    	 
+         tv.setText(sb.toString());        
+    }
+    
+    public void startMonitoringClipboard() {
+    	showMessage("Starting Monitoring ClipBoard");
+    	final ClipboardManager cm = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+    	    	       	
+    	// Add a clipboard listener. 
+    	cm.addPrimaryClipChangedListener(new OnPrimaryClipChangedListener() {
+			
+			public void onPrimaryClipChanged() {
+				// TODO Auto-generated method stub
+				if(cm.getPrimaryClipDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)) {
+		    		showMessage("Clipboard has data");
+		    		
+		    		ClipData cd = cm.getPrimaryClip();
+		    		int count = cd.getItemCount(); 
+		    		for(int i = 0; i < count; ++i){
+		    			String new_clip = cd.getItemAt(i).getText().toString();
+		    			mClips.add(new_clip);
+		    		}
+		    		showLatestClipboard();
+
+		    	} else  {
+		    		showMessage("Clipboard has no text data");    		
+		    	}
+				
+			}
+		});
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
